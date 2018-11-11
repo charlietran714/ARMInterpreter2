364a3381abad7bd7253e5eea48f02b2c9f3d0412
charlie <charlie.tran@cuw.edu>
2018-11-11 13:15:15 -0600

    Implemented SUB, ADDI, SUBI

--- app/src/main/java/com/example/aweso/arminterpreter/Instruction.java
+++ app/src/main/java/com/example/aweso/arminterpreter/Instruction.java
@@ -17,28 +17,34 @@ public class Instruction
         this.destinationName = parts[0].trim(); //"X0
         this.theInputNames.add(parts[1].trim()); //"X1"
         this.theInputNames.add(parts[2].trim()); //"X2"
+
     }
 
     public void execute()
     {
+        Register destinationRegister = ARMap.findRegisterWithName(this.destinationName);
+        Register input2Register = ARMap.findRegisterWithName(theInputNames.get(0));
+        Register input1Register = ARMap.findRegisterWithName(theInputNames.get(1));
+        int immediateValue = Integer.parseInt(this.theInputNames.get(1));
+        
         if(this.magicTrick.equalsIgnoreCase("ADD"))
         {
-            Register destinationRegister = ARMap.findRegisterWithName(this.destinationName);
-            Register input2Register = ARMap.findRegisterWithName(theInputNames.get(0));
-            Register input1Register = ARMap.findRegisterWithName(theInputNames.get(1));
             destinationRegister.setValue(input2Register.getValue() + input1Register.getValue());
         }
         else if(this.magicTrick.equalsIgnoreCase("SUB"))
         {
             //write the code to Subtract input1 from input2 and store the result in destinationName
+            destinationRegister.setValue(input2Register.getValue() - input1Register.getValue());
         }
         else if(this.magicTrick.equalsIgnoreCase("ADDI"))
         {
             //write the code to ADD input2 to an immediate value and store the result in destinationName
+            destinationRegister.setValue(input2Register.getValue() + immediateValue);
         }
         else if(this.magicTrick.equalsIgnoreCase("SUBI"))
         {
             //write the code to Subtract the immediate value in input1 from input2 and store the result in destinationName
+            destinationRegister.setValue(input2Register.getValue() - immediateValue);
         }
     }
 }
